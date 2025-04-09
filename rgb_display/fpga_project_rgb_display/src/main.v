0db186158f4891fd09dde559f10caf4cebd276bb
Alexandra Leah Kirakosian <160418036+leleah@users.noreply.github.com>
2025-04-09 17:34:19 +0300

    Add files via upload

---
+++ rgb_display/fpga_project_rgb_display/src/main.v
@@ -0,0 +1,48 @@
+module main(
+input Clock,
+output reg [4:0] Red,
+output reg  [5:0] Green,
+output reg  [4:0] Blue,
+output reg  Vsync,
+output reg  Hsync,
+output reg  Pixel_Clock,
+output reg  DEN
+);
+
+always @(posedge Clock) begin
+
+
+end
+
+endmodule
+
+module Hsync_Vsync(
+input CLKIN,
+output reg Vsync,
+output reg Hsync
+);
+reg [3:0]  counter;
+always @(negedge CLKIN) begin
+if (Hsync == 1)
+    Hsync <= 0;
+if (Hsync ==0)
+    counter <=1;
+    if (counter <40)
+        counter <= counter + 1'd1;
+    if (counter >=40)
+        Hsync <=1;
+end 
+
+reg [3:0]  counter2;
+always @(posedge Hsync)
+begin
+    Vsync <= 0;
+      counter2 <= 1;
+    if (Vsync == 0)
+        counter2 <= counter2 + 1'd1;
+    if (counter2 > 20)
+        Vsync <= 1;
+        
+end
+
+endmodule
